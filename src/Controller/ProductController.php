bd7dec6c201cd18a51411f52eb2c509f7dd392e4
linux <jennigonzalez99asdfghj@gmail.com>
2019-05-20 17:34:54 +0200

    actualizatacions

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -16,6 +16,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
+
 class ProductController extends AbstractController
 {
 
@@ -41,69 +42,34 @@ class ProductController extends AbstractController
     }
 
     /**
-     * Función para añadir un nuevo producto (que sea fruta)
+     * Función para añadir producto (que sea fruta)
      * @Route("/frutas/newproduct", name="new_product_fruit")
      */
     public function addProduct_Fruit(Request $request){
-        //crear un nuevo objeto producto
-        $product = new Product();
-
-        //como este método lo utilizamos para añadir productos que son FRUTAS
-        //le pondremos por defecto el campo isfruta en true
-        $isfruit=true;
-        $product->setIsfruit($isfruit);
-
-        //creamos el formulario
-        $form=$this->createForm(ProductType::class,$product);
-        $form->handleRequest($request);
-
-        $error=$form->getErrors();
-
-        if($form->isSubmitted() && $form->isValid()){
-            //capturo los datos
-            $product=$form->getData();
-
-            //para obtener la imagen
-            $file = $form->get('image')->getData();
-            if($file){
-                $fileName = $this->generateUniqueFileName().'.'.$file->guessExtension();
-                // moves the file to the directory where brochures are stored
-                try{
-                    $file->move(
-                        $this->getParameter('pictures_directory'),
-                        $fileName
-                    );
-                    //updates property to store picture
-                    $product->setImage($fileName);
-                }catch (FileException $e){
-                    $this->addFlash('warning','Error uploading image');
-                }
-                $product->setImage($fileName);
-            }
-
-            $entityManager=$this->getDoctrine()->getManager();
-            $entityManager->persist($product);
-            $entityManager->flush();
-            $this->addFlash('success','Producto creado correctamente');
-            return $this->redirectToRoute('app_fruits');
-        }
-        //renderizar formulario
-        return $this->render('fruit/new_product.html.twig', [
-            'error'=>$error,
-            'form'=>$form->createView()
-        ]);
+        return $this->addProduct($request, true, 'fruit/new_product.html.twig', 'app_fruits');
     }
     /**
-     * Función para añadir un nuevo producto (que sea verdura)
+     * Función para añadir producto (que sea verdura)
      * @Route("verduras/new_product", name="new_product_vegetable")
      */
     public function addProduct_Vegetable(Request $request){
+        return $this->addProduct($request, false, 'vegetable/new_product.html.twig', 'app_vegetables');
+    }
+
+    /**
+     * Función privada para añadir productos
+     * @param Request $request
+     * @param bool $isfruit
+     * @param string $template
+     * @param string $route
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    private function addProduct(Request $request, bool $isfruit, string $template, string $route){
         //crear un nuevo objeto producto
         $product = new Product();
 
         //como este método lo utilizamos para añadir productos que son VERDURAS
         //le pondremos por defecto el campo isfruta en false
-        $isfruit=false;
         $product->setIsfruit($isfruit);
 
         //creamos el formulario
@@ -118,6 +84,7 @@ class ProductController extends AbstractController
             //para obtener la imagen
             $file = $form->get('image')->getData();
             if($file){
+                //genero una serie de letras y números únicos + su extensión para la imagen
                 $fileName = $this->generateUniqueFileName().'.'.$file->guessExtension();
                 // moves the file to the directory where brochures are stored
                 try{
@@ -125,7 +92,7 @@ class ProductController extends AbstractController
                         $this->getParameter('pictures_directory'),
                         $fileName
                     );
-                    //updates property to store picture
+                    //actualizar propiedad de image
                     $product->setImage($fileName);
                 }catch (FileException $e){
                     $this->addFlash('warning','Error uploading image');
@@ -138,10 +105,10 @@ class ProductController extends AbstractController
             $entityManager->persist($product);
             $entityManager->flush();
             $this->addFlash('success','Producto creado correctamente');
-            return $this->redirectToRoute('app_vegetables');
+            return $this->redirectToRoute($route);
         }
         //renderizar formulario
-        return $this->render('vegetable/new_product.html.twig', [
+        return $this->render($template, [
             'error'=>$error,
             'form'=>$form->createView()
         ]);
@@ -152,45 +119,9 @@ class ProductController extends AbstractController
      * @Route("frutas/edit_product/{id}", name="edit_product_fruit")
      */
     public function editProduct_Fruit($id,Request $request){
-        $product=$this->getDoctrine()->getRepository(Product::class)->findBy(array('id'=>$id));
-        $prducttoedit=$product[0];
 
-        $form=$this->createForm(ProductEditType::class,$prducttoedit);
+        return $this->editProduct($id, $request, 'fruit/edit_product.html.twig', 'app_fruits');
 
-        $form->handleRequest($request);
-        $error=$form->getErrors();
-
-        if($form->isSubmitted() && $form->isValid()){
-            //para obtener la imagen
-            $file = $form->get('image')->getData();
-            if($file){
-                $fileName = $this->generateUniqueFileName().'.'.$file->guessExtension();
-                // moves the file to the directory where brochures are stored
-                try{
-                    $file->move(
-                        $this->getParameter('pictures_directory'),
-                        $fileName
-                    );
-                    //updates property to store picture
-                    $product->setImage($fileName);
-                }catch (FileException $e){
-                    $this->addFlash('warning','Error uploading image');
-                }
-                $product->setImage($fileName);
-            }
-
-            $entityManager=$this->getDoctrine()->getManager();
-            $entityManager->flush();
-
-            $this->addFlash('success', 'Producto modificado correctamente');
-            return $this->redirectToRoute('app_fruits');
-        }
-
-        return $this->render('fruit/edit_product.html.twig',[
-            'error'=>$error,
-            'products'=>$product,
-            'form'=>$form->createView()
-        ]);
     }
 
     /**
@@ -198,6 +129,20 @@ class ProductController extends AbstractController
      * @Route("verdura/edit_product/{id}", name="edit_product_vegetable")
      */
     public function editProduct_Vegetable($id,Request $request){
+
+        return $this->editProduct($id, $request, 'vegetable/edit_product.html.twig', 'app_vegetables');
+
+    }
+
+    /**
+     * Función para editar productos
+     * @param int $id
+     * @param Request $request
+     * @param string $template
+     * @param string $route
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    private function editProduct(int $id, Request $request, string $template, string $route){
         $product=$this->getDoctrine()->getRepository(Product::class)->findBy(array('id'=>$id));
         $prducttoedit=$product[0];
 
@@ -212,43 +157,42 @@ class ProductController extends AbstractController
             $entityManager->flush();
 
             $this->addFlash('success', 'Producto modificado correctamente');
-            return $this->redirectToRoute('app_vegetables');
+            return $this->redirectToRoute($route);
         }
 
-        return $this->render('vegetable/edit_product.html.twig',[
+        return $this->render($template,[
             'error'=>$error,
             'products'=>$product,
             'form'=>$form->createView()
         ]);
     }
-
     /**
      * Función para eliminar producto - fruta
      * @Route("/fruta/delete/{id}", name="delete_product_fruit")
      */
     public function deleteProduct_Fruit($id, Request $request)
     {
-        //buscamos por el id de la fruta que hemos seleccionado para eliminar
-        $product=$this->getDoctrine()->getRepository(Product::class)->findBy(array('id'=>$id));
-        $producttodelete=$product[0];
-
-        $entityManager=$this->getDoctrine()->getManager();
-        //comando en cuestión que borrará el producto
-        $entityManager->remove($producttodelete);
-        $entityManager->flush();
-
-        $this->addFlash('success', 'Producto eliminado correctmanete');
-        //una vez eliminado,volvemos a la página del resto de las frutas, para comprobar que se ha borrado correctamente
-        return $this->redirectToRoute('app_fruits');
-
+        return $this->deleteProduct($request, $id, 'app_fruits');
     }
+
     /**
      * Función para eliminar producto - verdura
      * @Route("/verdura/delete/{id}", name="delete_product_vegetable")
      */
     public function deleteProduct_Vegetable($id, Request $request)
     {
-        //buscamos por el id de la verdura que hemos seleccionado para eliminar
+        return $this->deleteProduct($request, $id, 'app_vegetables');
+    }
+
+    /**
+     * Función para eliminar producto
+     * @param Request $request
+     * @param int $id
+     * @param string $route
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    private function deleteProduct(Request $request, int $id, string $route){
+        //buscamos por el id del producto que hemos seleccionado para eliminar
         $product=$this->getDoctrine()->getRepository(Product::class)->findBy(array('id'=>$id));
         $producttodelete=$product[0];
 
@@ -258,30 +202,38 @@ class ProductController extends AbstractController
         $entityManager->flush();
 
         $this->addFlash('success', 'Producto eliminado correctmanete');
-        //una vez eliminado,volvemos a la página del resto de las frutas, para comprobar que se ha borrado correctamente
-        return $this->redirectToRoute('app_vegetables');
-
+        //una vez eliminado,volvemos a la página que indicamos por parámetros, para comprobar que se ha borrado correctamente
+        return $this->redirectToRoute($route);
     }
+
     /**
      * Función para ver un producto seleccionado - fruta
      * @Route("fruta/view_product/{id}", name="view_product_fruit")
      */
     public function viewProduct_Fruit($id){
-        $product = $this->getDoctrine()->getRepository(Product::class)->find($id);
 
-        return $this->render('fruit/view_product.html.twig', [
-            'product' => $product
-
-        ]);
+        return $this->viewProduct($id, 'fruit/view_product.html.twig');
     }
     /**
      * Función para ver un producto seleccionado - verdura
      * @Route("verdura/view_product/{id}", name="view_product_vegetable")
      */
     public function viewProduct_Vegetable($id){
+
+        return $this->viewProduct($id, 'vegetable/view_product.html.twig');
+    }
+
+    /**
+     * Función para ver un producto en concreto
+     * @param $id
+     * @param string $template
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    private function viewProduct($id, string $template){
+
         $product = $this->getDoctrine()->getRepository(Product::class)->find($id);
 
-        return $this->render('vegetable/view_product.html.twig', [
+        return $this->render($template, [
             'product' => $product
 
         ]);
